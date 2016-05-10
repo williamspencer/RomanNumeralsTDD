a89f1dfa5390f23d9f5a50c0d4afa5d2703e16db
williamspencer <badgley@gmail.com>
2016-05-09 22:45:21 -0400

    refactored, added to arrays

--- src/Roman.java
+++ src/Roman.java
@@ -1,27 +1,17 @@
 
 public class Roman {
 
-public static String intToRoman(int value){
-	String roman = "";
-	int [] arabicNum = {10,9};
-	String [] romanNum = {"X","IX"};
-	
-	for(int i = 0; i < arabicNum.length; i++){
-		while (value >= arabicNum[i]){
-			roman += romanNum[i];
-			value -= arabicNum[i];
+	public static String intToRoman(int value) {
+		String roman = "";
+		int[] arabicNum = { 10, 9, 5, 4, 1 };
+		String[] romanNum = { "X", "IX", "V", "IV", "I" };
+
+		for (int i = 0; i < arabicNum.length; i++) {
+			while (value >= arabicNum[i]) {
+				roman += romanNum[i];
+				value -= arabicNum[i];
+			}
 		}
+		return roman;
 	}
-
-	if (value == 4){
-		roman = "IV";
-		value -= 4;
-	}
-	while (value > 0){
-		roman += "I";
-		value--;
-	}
-	return roman;
-
-}
 }
